e68173d11dd9c2b8b69bd7c18b4b10eb18d55602
davidpierce24 <pie2ce@gmail.com>
2022-08-18 11:20:03 -0400

    sidebar icon updates

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,18 +1,24 @@
-
+import { FaLinkedin, FaGithub  } from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
 
 const SideBar = () => {
 
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 
                         flex flex-col
-                        bg-gray-900 text-white">
-            <i>A</i>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
+                        bg-gray-900 text-white shadow-lg">
+            <SideBarIcon icon={ <FaLinkedin size='28' /> } />
+            <SideBarIcon icon={ <FaGithub size='28' /> } />
+            <SideBarIcon icon={ <HiOutlineMail size='28' /> } />
 
         </div>
     )
-}
+};
+
+const SideBarIcon = ({icon}) => (
+    <div className='sidebar-icon'>
+        {icon}
+    </div>
+);
 
 export default SideBar;
