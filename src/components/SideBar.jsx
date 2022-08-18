73ef50fb2b7e630458f532c654ac77c57f2d5418
davidpierce24 <pie2ce@gmail.com>
2022-08-18 13:46:05 -0400

    sidebar icons updates

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,13 +4,13 @@ import { HiOutlineMail } from 'react-icons/hi';
 const SideBar = () => {
 
     return (
-        <div className="fixed top-0 left-0 h-screen w-16 m-0 
+        <div className="fixed top-0 left-0 h-screen w-16 ml-4 
                         flex flex-col justify-center
-                        bg-gray-900 bg-opacity-40  text-white shadow-lg">
-            <div>
-                <SideBarIcon icon={ <FaLinkedin size='28' /> } />
-                <SideBarIcon icon={ <FaGithub size='28' /> } />
-                <SideBarIcon icon={ <HiOutlineMail size='28' /> } />
+                        bg-gray-900 bg-opacity-0  text-white shadow-lg">
+            <div className='height-auto bg-gray-300 bg-opacity-20 rounded-xl pt-2 pb-2 backdrop-blur-sm'>
+                <SideBarIcon icon={ <FaLinkedin size='28' /> } text={ 'LinkedIn' } />
+                <SideBarIcon icon={ <FaGithub size='28' /> } text={ 'GitHub' } />
+                <SideBarIcon icon={ <HiOutlineMail size='28' /> } text={ 'Contact Me' } />
             </div>
             
 
@@ -18,9 +18,13 @@ const SideBar = () => {
     )
 };
 
-const SideBarIcon = ({icon}) => (
-    <div className='sidebar-icons'>
+const SideBarIcon = ({icon, text = 'tooltip'}) => (
+    <div className='sidebar-icons group'>
         {icon}
+
+        <span className='sidebar-tooltip group-hover:scale-100'>
+            {text}
+        </span>
     </div>
 );
 
