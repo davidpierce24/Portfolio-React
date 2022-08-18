3f664abc411a3abbfaa8934282ac2bf19e80b5d4
davidpierce24 <pie2ce@gmail.com>
2022-08-18 14:45:11 -0400

    react router dom

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,15 +8,21 @@ const SideBar = () => {
                         flex flex-col justify-center
                         bg-gray-900 bg-opacity-0  text-white shadow-lg">
             <div className='height-auto bg-gray-300 bg-opacity-20 rounded-xl pt-2 pb-2 backdrop-blur-sm'>
-                <SideBarIcon icon={ <FaLinkedin size='28' /> } text={ 'LinkedIn' } />
-                <SideBarIcon icon={ <FaGithub size='28' /> } text={ 'GitHub' } />
-                <SideBarIcon icon={ <HiOutlineMail size='28' /> } text={ 'Contact Me' } />
+                <a href="https://www.linkedin.com/in/david-pierce-dev24/" target='_blank' rel="noreferrer noopener">
+                    <SideBarIcon icon={ <FaLinkedin size='28' /> } text={ 'LinkedIn' } />
+                </a>
+                <a href="https://github.com/davidpierce24" target='_blank' rel="noreferrer noopener">
+                    <SideBarIcon icon={ <FaGithub size='28' /> } text={ 'GitHub' } />
+                </a>
+                <a href="mailto:pie2ce@gmail.com" target='_blank' rel="noreferrer noopener">
+                    <SideBarIcon icon={ <HiOutlineMail size='28' /> } text={ 'Contact Me' } />
+                </a>
             </div>
             
-
         </div>
     )
 };
+
 
 const SideBarIcon = ({icon, text = 'tooltip'}) => (
     <div className='sidebar-icons group'>
