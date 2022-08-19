2e7e2b81b9b959e0e3d6a3cd004ae9ba65edf3bf
davidpierce24 <pie2ce@gmail.com>
2022-08-19 01:58:19 -0400

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import LandingPage from "./components/LandingPage";
 import Navbar from "./components/NavBar";
+import ProjectsPage from "./components/ProjectsPage";
 import SideBar from "./components/SideBar";
 
 
@@ -11,12 +12,7 @@ function App() {
       <Navbar />
       <SideBar />
       <LandingPage />
-      
-      <div className='w-full h-screen bg-gradient-to-b from-pink-500 to-black'>
-        <p className='text-white pt-80'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti exercitationem porro dignissimos reprehenderit est error, at, illo ipsam eaque saepe officia quo natus, tenetur accusantium distinctio placeat veniam! Alias, provident?Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dignissimos ad incidunt, totam ipsam aliquam facere illo autem deleniti sequi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut amet tempora corrupti distinctio rerum. Ratione cupiditate porro accusamus exercitationem tempora molestiae suscipit doloribus expedita, perspiciatis provident maiores quis sint hic voluptatibus inventore, saepe, voluptate iusto temporibus. Consequatur culpa deserunt officia mollitia voluptatem obcaecati beatae sed assumenda unde labore. Quod, perspiciatis corrupti placeat totam soluta labore ratione officiis quis et odio aperiam mollitia dolores doloremque necessitatibus sapiente ipsa nesciunt minima, magni molestiae eveniet aliquam. Facere voluptate nihil debitis officia id ab corrupti, minima dolores placeat ea deleniti eos labore omnis quo quaerat similique laudantium totam molestias excepturi. Quis maxime magni, harum expedita explicabo magnam repellendus. Iusto ad tenetur culpa? Nisi at soluta praesentium numquam, illo sunt vel placeat sequi facere dignissimos, laudantium impedit perspiciatis unde aspernatur eius ab iure? Fugit amet fuga architecto vel esse corrupti iure quae ab quam maxime beatae qui voluptates ipsum, possimus earum magni itaque ipsam modi, impedit eaque non! Quis veniam explicabo magnam iusto harum quia! Atque excepturi veniam aspernatur in voluptate. Maxime magni voluptatibus ducimus perferendis unde quos aliquid reprehenderit asperiores placeat soluta quidem, repellat expedita officia deleniti quisquam tenetur recusandae ullam? Quis ipsa quasi ut a dolore quae fugit, nulla perspiciatis, vel doloremque cum.</p>
-      </div>
-      
-      
+      <ProjectsPage />
     </div>
   );
 }
---
+++ src/components/ProjectsPage.jsx
@@ -0,0 +1,12 @@
+
+
+const ProjectsPage = () => {
+
+    return (
+        <div className='w-full h-screen bg-gradient-to-b from-pink-500 to-black'>
+            
+        </div>
+    )
+}
+
+export default ProjectsPage;
