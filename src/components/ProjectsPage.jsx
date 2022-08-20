73b24eb3c38b91829719a61624f8f78669c0316f
davidpierce24 <pie2ce@gmail.com>
2022-08-19 22:35:09 -0400

    link updates

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -11,10 +11,14 @@ const ProjectsPage = () => {
             <p className="text-white font-semibold text-4xl sm:text-6xl p-10 pt-20 text-center font-jet-brains">Projects</p>
             <div className='flex justify-center gap-10 flex-wrap p-5 h-auto'>
                 
-                <ProjectCard video={ChampView} title={'ChampView'} text={''} tech={''}/>
-                <ProjectCard video={Vital} title={'Vital'} text={''} tech={''}/>
-                <ProjectCard video={Stocks} title={'Potential'} text={''} tech={''}/>
-                <ProjectCard video={Impression} title={'iMPReSSioN'} text={''} tech={''}/>
+                <ProjectCard video={ChampView} title={'ChampView'} 
+                text={''} tech={''}/>
+                <ProjectCard video={Vital} title={'Vital'} 
+                text={''} tech={''}/>
+                <ProjectCard video={Stocks} title={'Potential'} 
+                text={'A Python Machine Learning Stock Predictor'} tech={'Python, Anaconda, Pandas, Prophet Forcasting Model, Plotly, Streamlit'}/>
+                <ProjectCard video={Impression} title={'iMPReSSioN'} 
+                text={''} tech={''}/>
             </div>
         </div>
     )
@@ -22,7 +26,7 @@ const ProjectsPage = () => {
 
 const ProjectCard = ({video, title, text, tech}) => (
     <motion.div 
-        className='bg-blue-500 w-full sm:w-xl 2xl:w-2xl max-w-4xl h-64 sm:h-96 rounded-3xl bg-opacity-80 backdrop-blur-sm shadow-xl shadow-pink-800 hover:scale-105 duration-200 flex justify-between overflow-hidden'
+        className='bg-blue-500 w-full sm:w-xl 2xl:w-2xl max-w-4xl h-64 sm:h-96 rounded-3xl bg-opacity-80 backdrop-blur-sm shadow-xl shadow-pink-800  flex justify-between overflow-hidden'
         initial={{opacity:0}}
         whileInView={{opacity:1}}
         transition={{delay:0.05}}
@@ -30,10 +34,30 @@ const ProjectCard = ({video, title, text, tech}) => (
             <video controls autoPlay loop muted className='relative w-full sm:w-1/2 h-full object-cover'>
                 <source src={video}/>
             </video>
-            <div  className='invisible sm:visible w-1/2 h-full flex flex-col justify-start items-center p-5 font-jet-brains'>
+            <div  className='invisible sm:visible w-1/2 h-full flex flex-col justify-around items-center p-5 font-jet-brains'>
                 <p className='text-white text-2xl xl:text-4xl'>{title}</p>
+                <p className='text-white text-md md:text-2xl text-center mt-4'>{text}</p>
+                <p className='text-white text-md md:text-2xl text-center mt-4 '>{tech}</p>
+                {video == ChampView &&  
+                    <div className='flex gap-2'>
+                        <LinkButton name={'Go to the Site'} link={'https://lol-react-app.vercel.app/'} /><LinkButton name={'Check it Out On GitHub'} link={'https://github.com/davidpierce24/Champ-View-League-of_Legends_React_App'} />
+                    </div>  }
+                {video == Vital && <LinkButton name={'Check it Out On GitHub'} link={'https://github.com/davidpierce24/Vital_Gym-Finder_CSharp-FullStack-Project'} />}
+                {video == Impression && <LinkButton name={'Check it Out On GitHub'} link={'https://github.com/davidpierce24/iMPReSSioN-python-flask-full-stack'} />}
+                {video == Stocks && <LinkButton name={'Check it Out On GitHub'} link={'https://github.com/davidpierce24/machine_learning_stock_prediction_python'} />}
+
             </div>
     </motion.div>
+)
+
+const LinkButton = ({name, link}) => (
+    <a href={link} target='_blank' rel="noreferrer noopener"
+    ><button
+    type="button"
+    data-mdb-ripple="true"
+    data-mdb-ripple-color="light"
+    class="inline-block px-6 py-2.5 bg-pink-500 text-white font-semibold text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition  ease-in-out hover:scale-105 duration-150"
+  >{name}</button></a>
 )
 
 export default ProjectsPage;
